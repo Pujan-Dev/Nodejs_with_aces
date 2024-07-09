af0d1656b94761f8c6e63a519c3348baab4cf697
Pujan-Dev <Pujanneupaneop0907@gmail.com>
2024-07-09 09:46:17 +0545

    Update

--- app.js
+++ app.js
@@ -2,41 +2,57 @@ const express = require("express")
 const connectToDb = require("./database/databaseConnection");
 const Blog = require("./model/blogModel");
 const app = express()
+
+const { multer, storage } = require('./middleware/multerConfig')
+const uplaod = multer({ storage: storage })
+
 connectToDb();
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/",(req,res)=>{
-    res.send("<h1>This is the ok</h1>")
+app.get("/",async (req, res) => {
+    // res.send("<h1>This is the ok</h1>")
+    const blogs= await Blog.find() //Always returns arrays
+    if(blogs.length === 0){
+        res.send("No blogs")
+    }
+
+    res.render("./blog/home.ejs",{blogs:blogs})
 })
 
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
     const name = "Pujan Neupane"
-    res.render("about.ejs",{name})
+    res.render("about.ejs", { name })
 })
-app.get("/contact",(req,res)=>{
-    const ACES= "Aces Workshop"
-    res.render("contact.ejs",{ACES})
+app.get("/contact", (req, res) => {
+    const ACES = "Aces Workshop"
+    res.render("contact.ejs", { ACES })
 })
 
-app.get("/createblog",(req,res)=>{
+app.get("/createblog", (req, res) => {
 
 
     res.render("./blog/create.ejs")
 })
 
-app.post("/createblog",async (req,res)=>{ 
+app.post("/createblog",uplaod.single('image'), async (req, res) => {
+    const fileName = req.file.filename
+    console.log(fileName)
     console.log(req.body)
-    const {title,subtitle,description}=req.body
+    const { title, Subtitle, description } = req.body
     await Blog.create({
         title,
-        subtitle,
-        description
+        subtitle:Subtitle,
+        description,
+        image: fileName
     })
     res.send("Blog created successfully")
 })
+
+app.use(express.static("./storage"))
+
 app.listen(3000, () => {
     console.log("NODEJS project has started at port" + 3000)
 })
