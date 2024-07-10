7a2c6e4ff7e24c9a3f115685a565039ddf818892
Pujan-Dev <Pujanneupaneop0907@gmail.com>
2024-07-10 23:38:44 +0545

    CURD OPERATION IS DONE

--- app.js
+++ app.js
@@ -12,39 +12,70 @@ app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/",async (req, res) => {
+app.get("/", async (req, res) => {
     // res.send("<h1>This is the ok</h1>")
-    const blogs= await Blog.find() //Always returns arrays
-    if(blogs.length === 0){
+    const blogs = await Blog.find() //Always returns arrays
+    if (blogs.length === 0) {
         res.send("No blogs")
     }
 
-    res.render("./blog/home.ejs",{blogs:blogs})
+    res.render("./blog/home.ejs", { blogs: blogs })
 })
 
 app.get("/about", (req, res) => {
     const name = "Pujan Neupane"
     res.render("about.ejs", { name })
 })
+
+app.get("/blogs/:id", async (req, res) => {
+    const id = req.params.id;
+    const blog = await Blog.findById(id)
+    res.render("./blog/blogs.ejs", { blog });
+})
+
+
 app.get("/contact", (req, res) => {
     const ACES = "Aces Workshop"
     res.render("contact.ejs", { ACES })
 })
 
 app.get("/createblog", (req, res) => {
-
-
     res.render("./blog/create.ejs")
 })
 
-app.post("/createblog",uplaod.single('image'), async (req, res) => {
+app.get("/deleteblog/:id", async (req, res) => {
+    const id = req.params.id;
+    await Blog.findByIdAndDelete(id)
+    // res.send("Blog Deleted"
+    res.redirect("/")
+})
+app.get("/update/:id", async (req, res) => {
+    const id = req.params.id;
+    const blog = await Blog.findById(id)
+    res.render("blog/updateblog.ejs", { blog })
+})
+app.post("/update/:id", uplaod.single('image'), async (req, res) => {
+    const id = req.params.id;
+    const fileName = req.file.filename;
+    const { title, Subtitle, description } = req.body;
+    await Blog.findByIdAndUpdate(id, {
+        title,
+        subtitle: Subtitle,
+        description,
+        image: fileName
+    });
+    res.redirect("/")
+
+})
+
+app.post("/createblog", uplaod.single('image'), async (req, res) => {
     const fileName = req.file.filename
     console.log(fileName)
     console.log(req.body)
     const { title, Subtitle, description } = req.body
     await Blog.create({
         title,
-        subtitle:Subtitle,
+        subtitle: Subtitle,
         description,
         image: fileName
     })
